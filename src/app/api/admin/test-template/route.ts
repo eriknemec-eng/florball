1eda9d8e5fb1638b1173b3f1def77246b3492305
Erik Němec <eriknemec@192.168.68.56>
2026-04-12 18:26:35 +0200

    Fix NextAuth path for production build

--- src/app/api/admin/test-template/route.ts
+++ src/app/api/admin/test-template/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { sendEmail } from '@/lib/mailer';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { getDb } from '@/lib/db';
 
 export async function POST(req: Request) {
